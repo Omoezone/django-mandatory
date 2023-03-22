05cb658119f768ce849dca83160d7bffce4459f7
NoelDj <noel0045@stud.kea.dk>
2023-03-22 11:54:50 +0100

    models

--- django_mandatory/bank_app/models.py
+++ django_mandatory/bank_app/models.py
@@ -3,19 +3,19 @@ from django.contrib.auth.models import User
 from datetime import datetime
 
 class Customer(models.Model):
-   user = models.ForeignKey(User, on_delete=(models.CASCADE))
+   user = models.ForeignKey(User, on_delete=models.PROTECT)
    phone = models.CharField(max_length=15)
    rank = models.IntegerField(max_length=1)
 
 class Employee(models.Model):
-   user = models.ForeignKey(User, on_delete=(models.CASCADE))
+   user = models.ForeignKey(User, on_delete=models.PROTECT)
    phone = models.CharField(max_length=15)
 
 class Account(models.Model):
    number = models.IntegerField(max_length=10)
    balance = models.DecimalField(max_digits=10, decimal_places=2)
    is_loan = models.BooleanField(default=False)
-   customer = models.ForeignKey(Customer, on_delete=(models.DO_NOTHING))
+   customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
    # use a trigger when customer is deleted to prevent invalid customer id
 
 class Transaction(models.Model):
@@ -23,5 +23,5 @@ class Transaction(models.Model):
    amount = models.DecimalField(max_digits=10, decimal_places=2)
    description = models.CharField(max_length=255)
    date = models.DateTimeField(datetime.now)
-   account = models.ForeignKey(Account, on_delete=(models.DO_NOTHING))
+   account = models.ForeignKey(Account, on_delete=models.PROTECT)
    # use a trigger when account is deleted to prevent invalid account id
