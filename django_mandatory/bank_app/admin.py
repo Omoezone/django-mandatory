c019b830993b9d8a9ce9a2bcce936fa38c72c53b
NoelDj <noel0045@stud.kea.dk>
2023-04-05 13:32:02 +0200

    projekt

--- django_mandatory/bank_app/admin.py
+++ django_mandatory/bank_app/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import UID, Customer, Account, Transaction, Rank
+
+admin.site.register(UID)
+admin.site.register(Customer)
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(Rank)
