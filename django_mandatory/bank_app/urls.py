0ce3288830881539cad05fcfaafc9e1d42dd087f
NoelDj <noel0045@stud.kea.dk>
2023-05-06 22:27:39 +0200

    created customer pages and the staff dashboard

--- django_mandatory/bank_app/urls.py
+++ django_mandatory/bank_app/urls.py
@@ -5,7 +5,13 @@ from . import views
 app_name = "bank_app"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='a'),
 
+    path('customer_account/', views.customer_account, name='customer_account'),
+    path('staff_dashboard/', views.staff_dashboard, name='staff_dashboard'),
     path('customer_dashboard/', views.customer_dashboard, name='customer_dashboard'),
+    path('customer_transaction/', views.customer_transaction, name='customer_transaction'),
+    path('customer_account/', views.customer_account, name='customer_account'),
+    path('account/<int:pk>/', views.account_single, name='account_single')
+
 ]
--- django_mandatory/bank_app/views.py
+++ django_mandatory/bank_app/views.py
@@ -1,18 +1,53 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Customer, Account
 # Create your views here.
 
 
 @login_required
 def index(request):
     # Use this bit to authenticate between users and admins
+    if request.user.is_staff:
+        return HttpResponseRedirect('staff_dashboard')
+    else:
+        return HttpResponseRedirect('customer_dashboard')
     # Which will decide which dashboard to show
-    return HttpResponseRedirect('customer_dashboard')
+    # return HttpResponseRedirect('customer_dashboard')
+
+
+@login_required
+def staff_dashboard(request):
+    userList = User.objects.all()
+    context = {
+        'users': userList
+    }
+    return render(request, 'staff_dashboard.html', context)
 
 
 @login_required
 def customer_dashboard(request):
-    return render(request, 'customer_dashboard.html')
+    accountList = Account.objects.all()
+    context = {
+        'accounts': accountList
+    }
+    return render(request, 'customer_dashboard.html', context)
 
+
+@login_required
+def customer_transaction(request):
+    return render(request, 'customer_transaction.html')
+
+
+@login_required
+def customer_account(request):
+    return render(request, 'customer_account.html')
+
+def account_single(request, pk):
+    account = get_object_or_404(Account, user=request.user, pk=pk)
+    context = {
+        'account': account,
+    }
+
+    return render(request, 'account_single.html', context)
