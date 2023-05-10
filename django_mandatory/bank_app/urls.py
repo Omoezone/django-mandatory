7001f52462eefc53749ed840ceb3bce6ca3688f1
Nikolaj <nikolaj.hansen93@gmail.com>
2023-05-10 10:07:12 +0200

    Not working at the moment. Using rest framework but should be hardcoded instead.

--- django_mandatory/bank_app/urls.py
+++ django_mandatory/bank_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .api.views import CustomerList, CustomerDetail
 
 
 app_name = "bank_app"
@@ -8,4 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('customer_dashboard/', views.customer_dashboard, name='customer_dashboard'),
+
+    path('customers/', CustomerList.as_view(), name='customer-list'),
+    path('customers/<int:pk>/', CustomerDetail.as_view(), name='customer-detail'),
 ]
