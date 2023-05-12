8405e6d7b57ee226e15d63327d570d1ffabbced7
Nikolaj <nikolaj.hansen93@gmail.com>
2023-05-12 15:11:15 +0200

    added rest for rank, account and transaction

--- django_mandatory/bank_app/api.py
+++ django_mandatory/bank_app/api.py
@@ -1,8 +1,34 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
-from .models import Customer
-from .serializers import CustomerSerializer
-from .permissions import IsOwnerOrNoAccess
+from models import Rank
+from models import Account
+from models import Customer
+from models import Transaction
+from models import RankSerializer
+from models import AccountSerializer
+from serializers import CustomerSerializer
+from serializers import TransactionSerializer
+from permissions import IsOwnerOrNoAccess
+
+
+class RankList(generics.ListCreateAPIView):
+    queryset = Rank.objects.all()
+    serializer_class = RankSerializer
+
+
+class RankDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Rank.objects.all()
+    serializer_class = RankSerializer
+
+
+class AccountList(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
 
 
 class CustomerList(generics.ListCreateAPIView):
@@ -13,3 +39,13 @@ class CustomerList(generics.ListCreateAPIView):
 class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+
+class TransactionList(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = TransactionSerializer
+
+
+class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = TransactionSerializer
--- django_mandatory/bank_app/serializers.py
+++ django_mandatory/bank_app/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
-from .models import Customer
+from models import Rank
+from models import Account
+from models import Customer
+from models import Transaction
+
+
+class RankSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', '')
+        model = Rank
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', '')
+        model = Account
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'user', 'phone', 'rank', 'full_name', 'can_make_loan', 'default_account')
         model = Customer
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', '')
+        model = Transaction
