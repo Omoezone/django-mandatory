e2423bd886c483a503cdfaa0a85068a7ba9697b1
Nikolaj <nikolaj.hansen93@gmail.com>
2023-05-17 04:03:19 +0200

    comment out breaking code

--- django_mandatory/bank_app/api.py
+++ django_mandatory/bank_app/api.py
@@ -9,9 +9,9 @@ class RankList(generics.ListCreateAPIView):
     queryset = Rank.objects.all()
     serializer_class = RankSerializer
 
-    def get_queryset(self):
-        queryset = Rank.objects.filter(user=self.request.user)
-        return queryset
+#    def get_queryset(self):
+#        queryset = Rank.objects.filter(user=self.request.user)
+#        return queryset
 
 
 class RankDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -23,9 +23,9 @@ class AccountList(generics.ListCreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
-    def get_queryset(self):
-        queryset = Account.objects.filter(user=self.request.user)
-        return queryset
+#    def get_queryset(self):
+#        queryset = Account.objects.filter(user=self.request.user)
+#        return queryset
 
 
 class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -37,9 +37,9 @@ class CustomerList(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
-    def get_queryset(self):
-        queryset = Customer.objects.filter(user=self.request.user)
-        return queryset
+#    def get_queryset(self):
+#        queryset = Customer.objects.filter(user=self.request.user)
+#        return queryset
 
 
 class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -51,9 +51,9 @@ class TransactionList(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = TransactionSerializer
 
-    def get_queryset(self):
-        queryset = Rank.objects.filter(user=self.request.user)
-        return queryset
+#    def get_queryset(self):
+#        queryset = Rank.objects.filter(user=self.request.user)
+#        return queryset
 
 
 class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
