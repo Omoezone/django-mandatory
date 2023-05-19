fa41090f1f086b77a360abb7c6b7971300609dcf
Nikolaj <nikolaj.hansen93@gmail.com>
2023-05-19 10:33:40 +0200

    added django user model to api

--- django_mandatory/bank_app/serializers.py
+++ django_mandatory/bank_app/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Rank, Account, Customer, Transaction
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'is_staff', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'is_staff': {'read_only': True}
+        }
 
 
 class RankSerializer(serializers.ModelSerializer):
