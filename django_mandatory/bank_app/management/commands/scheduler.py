3b9d21f7180e939ea33eaf92f7ec1007d496f9b8
NoelDj <noel0045@stud.kea.dk>
2023-06-05 10:59:21 +0200

    scheduler finished

--- django_mandatory/bank_app/management/commands/scheduler.py
+++ django_mandatory/bank_app/management/commands/scheduler.py
@@ -1,21 +1,33 @@
 import schedule
 import time
+from django.core.management.base import BaseCommand
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from bank_app.models import Customer, Rank
+from django.utils import timezone
 
 
-
-def update_rank():
-    print('checking for customer that should have their rank updated')
-    rank_silver = Rank.objects.get(pk=4)
-    customers = Customer.objects.filter(rank__value=10)
-    for customer in customers:
-        customer.rank = rank_silver
-        customer.save()
+class Command(BaseCommand):
+    def handle(self, **options):
+        self.schedule_update_rank()
 
 
-schedule.every(1).seconds.do(update_rank)
+    def update_rank(self):
+        print('Checking for customers that should have their rank updated')
+        rank_silver = Rank.objects.get(pk=4)
+        one_day_ago = timezone.now() - timezone.timedelta(days=1)
+        #one_month_ago = datetime.now() - relativedelta(months=1)
+        customers = Customer.objects.filter(user__date_joined__gte=one_day_ago, rank__value=10)
+        for customer in customers:
+            print(f"Updating customer rank for {customer.user.username}")
+            customer.rank = rank_silver
+            customer.save()
 
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    def schedule_update_rank(self):
+        interval = 1
+        schedule.every(interval).seconds.do(self.update_rank)
+        #schedule.every(interval).months.do(self.update_rank)
+        while True:
+            schedule.run_pending()
+            time.sleep(interval)
